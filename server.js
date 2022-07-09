87a91ace99fe9a72917acb2e5b20f8362a3fe2c2
Jake Schaefbauer <jschaefmn@gmail.com>
2022-07-09 10:38:58 -0500

    still fixing issues with heroku

--- server.js
+++ server.js
@@ -25,3 +25,9 @@ app.get('/notes', (req, res) => {
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
+
+
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
