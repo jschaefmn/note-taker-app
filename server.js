d233565321e83a02f10eb38b7751d54ecd645e0e
Jake Schaefbauer <jschaefmn@gmail.com>
2022-07-09 10:32:58 -0500

    fixing heroku launching issues

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-const notes = require('./db/db.jsob');
+const notes = require('./data/db.json');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
