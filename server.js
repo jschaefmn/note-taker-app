37b214a54a0e58c750332d309f5fb673eb031bfd
Jake Schaefbauer <jschaefmn@gmail.com>
2022-07-09 10:25:29 -0500

    Add heroku

---
+++ server.js
@@ -0,0 +1,27 @@
+const PORT = process.env.PORT || 3001;
+const fs = require('fs');
+const path = require('path');
+
+const express = require('express');
+const app = express();
+
+const notes = require('./db/db.jsob');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('api/notes', (req, res) => {
+  res.json(notes.slice(1));
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
