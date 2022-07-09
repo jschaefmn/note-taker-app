336ceaa8a4b5aaffe22eda4c5e93cee1bd9a62f9
Jake Schaefbauer <jschaefmn@gmail.com>
2022-07-09 11:58:30 -0500

    fixed bugs with saving notes

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('api/notes', (req, res) => {
+app.get('/api/notes', (req, res) => {
   res.json(notes.slice(1));
 });
 
@@ -26,7 +26,50 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+function createNewNote(body, notesArray) {
+  const newNote = body;
+  if (!Array.isArray(notesArray))
+    notesArray = [];
 
+  if (notesArray.length === 0)
+    notesArray.push(0);
+
+  body.id = notesArray[0];
+  notesArray[0]++;
+
+  notesArray.push(newNote);
+  fs.writeFileSync(
+    path.join(__dirname, './data/db.json'),
+    JSON.stringify(notesArray, null, 2)
+  );
+  return newNote;
+}
+
+app.post('/api/notes', (req, res) => {
+  const newNote = createNewNote(req.body, notes);
+  res.json(newNote);
+});
+
+function deleteNote(id, notesArray) {
+  for (let i = 0; i < notesArray.length; i++) {
+    let note = notesArray[i];
+
+    if (note.id == id) {
+      notesArray.splic(i, 1);
+      fs.writeFileSync(
+        path.join(__dirname, './data/db.json'),
+        JSON.stringify(notesArray, null, 2)
+      );
+
+      break
+    }
+  }
+}
+
+app.delete('/api/notes/:id', (req, res) => {
+  deleteNote(req.params.id, notes);
+  res.json(true);
+});
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
